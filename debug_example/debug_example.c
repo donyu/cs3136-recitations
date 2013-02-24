18a40597719e0e13fe49c95d5ad502f3d615d766
Don Yu <dy2212@columbia.edu>
2013-02-24 18:53:12 -0500

    debugging section added

---
+++ debug_example/debug_example.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void print_array(int nums[], int n) {
+  int i;
+  for(i = 0; i < n; i++) {
+    printf("%d ", nums[i]);
+  }
+
+  fflush(stdout);
+}
+
+void overwrite_array(int orig[], int orig_size, int copy[], int copy_size) {
+  int i = 0;
+  while(i < orig_size) {
+    orig[i++] = copy[i++];
+  }
+}
+
+int main() {
+  int orig[] = {1, 2, 3, 4, 5};
+  int copy[] = {6, 7, 8, 9, 10};
+  print_array(orig, 5);
+  printf("\n");
+  print_array(copy, 5);
+  printf("\n");
+
+  overwrite_array(orig, 5, copy, 5);
+  print_array(orig, 5);
+  printf("\n");
+
+  return 0;
+}
